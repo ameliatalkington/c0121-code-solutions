30eb1718cad58469ffbee6777764362faf5012f1
ameliatalkington <amrtalk@stanford.edu>
2021-01-29 18:26:37 -0700

    Removed extra variable

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,12 +1,11 @@
 /* exported swapChars */
 function swapChars(firstIndex, secondIndex, string) {
-  var charHolder = firstIndex;
   var newString = '';
   for (var i = 0; i < string.length; i++) {
     if (i === firstIndex) {
       newString += string[secondIndex];
     } else if (i === secondIndex) {
-      newString += string[charHolder];
+      newString += string[firstIndex];
     } else {
       newString += string[i];
     }
