6252cdf119a506c33c29144440478414b4ad0df5
ameliatalkington <amrtalk@stanford.edu>
2021-02-03 12:31:12 -0700

    changes

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -18,5 +18,13 @@ var $inputEmail = $inputArray[1];
 var $textcontent = document.querySelector('textarea');
 
 $inputName.addEventListener('focus', handleFocus);
+$inputName.addEventListener('blur', handleBlur);
+$inputName.addEventListener('input', handleInput);
+
+$inputEmail.addEventListener('focus', handleFocus);
 $inputEmail.addEventListener('blur', handleBlur);
+$inputEmail.addEventListener('input', handleInput);
+
+$textcontent.addEventListener('focus', handleFocus);
+$textcontent.addEventListener('blur', handleBlur);
 $textcontent.addEventListener('input', handleInput);
