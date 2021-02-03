63305633e4f621b306906071b02f805439d64ff8
ameliatalkington <amrtalk@stanford.edu>
2021-02-03 12:42:49 -0700

    Completed JS Modal - AT

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,25 +1,18 @@
-var $buttonClick = document.querySelectorAll('.button');
-var $buttonOpen = $buttonClick[0];
-var $buttonClose = $buttonClick[1];
+var $buttonClick = document.querySelector('.button');
 var $closeButton = document.querySelector('.button.close');
 var $modalBox = document.querySelector('.modal-box');
-var isOpen = false;
 
 function click(event) {
-  if ($buttonOpen.className === 'button open') {
+  if ($buttonClick.className === 'button open') {
     $modalBox.className = 'modal-box on';
-    isOpen = true;
   }
-  if ($buttonClick.className === 'button close') {
-    $modalBox.className = 'modal-box off';
-    isOpen = false;
-  }
+}
 
-  if (isOpen === false) {
-    $buttonClick.className = 'button open';
-  } else {
-    $buttonClick = $closeButton;
+function close(event) {
+  if ($closeButton.className === 'button close') {
+    $modalBox.className = 'modal-box off';
   }
 }
 
 $buttonClick.addEventListener('click', click);
+$closeButton.addEventListener('click', close);
