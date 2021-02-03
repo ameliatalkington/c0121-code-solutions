74ab30f4bfc3e753045203f2b52f6e9a508d9f98
ameliatalkington <amrtalk@stanford.edu>
2021-02-03 11:30:24 -0700

    in progress 2

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,11 +1,22 @@
 var $buttonClick = document.querySelector('.button');
+var $closeButton = document.querySelector('.button.close');
 var $modalBox = document.querySelector('.modal-box');
+var isOpen = false;
 
 function click(event) {
   if ($buttonClick.className === 'button open') {
     $modalBox.className = 'modal-box on';
-  } else if ($buttonClick.className === 'button close') {
+    isOpen = true;
+  }
+  if ($buttonClick.className === 'button close') {
     $modalBox.className = 'modal-box off';
+    isOpen = false;
+  }
+
+  if (isOpen === false) {
+    $buttonClick.className = 'button open';
+  } else {
+    $buttonClick = $closeButton;
   }
 }
 
