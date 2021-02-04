700569182cd2e05fbe25c1d5ac1971e7e5de1cd5
ameliatalkington <amrtalk@stanford.edu>
2021-02-04 15:39:19 -0700

    Cleaning up code

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -3,67 +3,68 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 function cardGame(players, cards) {
 
   var shuffledCards = _.shuffle(cardCollection());
+  console.log(shuffledCards);
 
   var playersCollection = {};
   for (var i = 0; i < players.length; i++) {
     playersCollection[players[i]] = calculateScore((shuffledCards.splice(0, cards)));
   }
 
-  var max = 0;
-  var runoffPlayers = [];
-  for (var player in playersCollection) {
-    if (playersCollection[player] > max) {
-      max = playersCollection[player];
-      runoffPlayers[0] = player;
-    } else if (playersCollection[player] === max) {
-      runoffPlayers.push(player);
-    }
-  }
+  // var max = 0;
+  // var runoffPlayers = [];
+  // for (var player in playersCollection) {
+  //   if (playersCollection[player] > max) {
+  //     max = playersCollection[player];
+  //     runoffPlayers[0] = player;
+  //   } else if (playersCollection[player] === max) {
+  //     runoffPlayers.push(player);
+  //   }
+  // }
 
-  if (runoffPlayers.length > 1) {
-    cardGame(runoffPlayers, cards);
-  } else {
-    console.log(runoffPlayers[0], 'is the WINNER! With a score of:', max);
-  }
+  // if (runoffPlayers.length > 1) {
+  //   cardGame(runoffPlayers, cards);
+  // } else {
+  //   console.log(runoffPlayers[0], 'is the WINNER! With a score of:', max);
+  // }
 
 }
 
 function cardCollection() {
-  var cardsCollection = [];
+  var arrayOfCardObjects = [];
   var rank = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'];
   var suit = ['hearts', 'spades', 'diamonds', 'clubs'];
   for (var j = 0; j < suit.length; j++) {
+    var cardObject = {};
     for (var k = 0; k < rank.length; k++) {
-      var cardObject = {};
       cardObject[suit[j]] = rank[k];
-      cardsCollection.push(cardObject);
+      arrayOfCardObjects.push(cardObject);
     }
   }
-  return cardsCollection;
+  console.log(arrayOfCardObjects);
+  return arrayOfCardObjects;
 }
 
-function calculateScore(arrayOfObjects) {
-  var count = 0;
-  for (var index = 0; index < arrayOfObjects.length; index++) {
-    var objectOfCards = arrayOfObjects[index];
-    for (var keys in objectOfCards) {
-      if (objectOfCards[keys] === 'ace') {
-        count += 11;
-      } else if (objectOfCards[keys] === 'jack' || objectOfCards[keys] === 'queen' || objectOfCards[keys] === 'king') {
-        count += 10;
-      } else {
-        count += objectOfCards[keys];
-      }
-    }
-  }
-  return count;
-}
+// function calculateScore(arrayOfCardObjects) {
+//   var count = 0;
+//   for (var index = 0; index < arrayOfCardObjects.length; index++) {
+//     var objectOfCards = arrayOfCardObjects[index];
+//       if (objectOfCards[keys] === 'ace') {
+//         count += 11;
+//       } else if (objectOfCards[keys] === 'jack' || objectOfCards[keys] === 'queen' || objectOfCards[keys] === 'king') {
+//         count += 10;
+//       } else {
+//         count += objectOfCards[keys];
+//       }
+//   }
+//   console.log(objectOfCards);
+//   return count;
+// }
 
 var competitors = ['Amelia', 'Louis', 'TJ', 'Cody', 'Tim'];
 var numOfCards1 = 6;
-var numOfCards2 = 5;
-var numOfCards3 = 7;
+// var numOfCards2 = 5;
+// var numOfCards3 = 7;
 
 cardGame(competitors, numOfCards1);
-cardGame(competitors, numOfCards2);
-cardGame(competitors, numOfCards3);
+// cardGame(competitors, numOfCards2);
+// cardGame(competitors, numOfCards3);
