d2d1230fc16b28113719ee8c4c414d965f2a25b4
ameliatalkington <amrtalk@stanford.edu>
2021-02-04 16:42:33 -0700

    Changed code structure and variable names - AT

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -3,30 +3,31 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 function cardGame(players, cards) {
 
   var shuffledCards = _.shuffle(cardCollection());
-  console.log(shuffledCards);
 
-  var playersCollection = {};
+  var playerScoreArray = [];
   for (var i = 0; i < players.length; i++) {
-    playersCollection[players[i]] = calculateScore((shuffledCards.splice(0, cards)));
+    var playersCollection = {};
+    playersCollection.name = players[i];
+    playersCollection.score = calculateScore((shuffledCards.splice(0, cards)));
+    playerScoreArray.push(playersCollection);
   }
 
-  // var max = 0;
-  // var runoffPlayers = [];
-  // for (var player in playersCollection) {
-  //   if (playersCollection[player] > max) {
-  //     max = playersCollection[player];
-  //     runoffPlayers[0] = player;
-  //   } else if (playersCollection[player] === max) {
-  //     runoffPlayers.push(player);
-  //   }
-  // }
+  var max = 0;
+  var runoffPlayers = [];
+  for (var playerIndex = 0; playerIndex < playerScoreArray.length; playerIndex++) {
+    if (playerScoreArray[playerIndex].score > max) {
+      max = playerScoreArray[playerIndex].score;
+      runoffPlayers[0] = playerScoreArray[playerIndex].name;
+    } else if (playerScoreArray[playerIndex].score === max) {
+      runoffPlayers.push(playerScoreArray[playerIndex].name);
+    }
+  }
 
-  // if (runoffPlayers.length > 1) {
-  //   cardGame(runoffPlayers, cards);
-  // } else {
-  //   console.log(runoffPlayers[0], 'is the WINNER! With a score of:', max);
-  // }
-
+  if (runoffPlayers.length > 1) {
+    cardGame(runoffPlayers, cards);
+  } else {
+    console.log(runoffPlayers[0], 'is the WINNER! With a score of:', max);
+  }
 }
 
 function cardCollection() {
@@ -34,37 +35,35 @@ function cardCollection() {
   var rank = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'];
   var suit = ['hearts', 'spades', 'diamonds', 'clubs'];
   for (var j = 0; j < suit.length; j++) {
-    var cardObject = {};
     for (var k = 0; k < rank.length; k++) {
-      cardObject[suit[j]] = rank[k];
+      var cardObject = {};
+      cardObject.suit = suit[j];
+      cardObject.rank = rank[k];
       arrayOfCardObjects.push(cardObject);
     }
   }
-  console.log(arrayOfCardObjects);
   return arrayOfCardObjects;
 }
 
-// function calculateScore(arrayOfCardObjects) {
-//   var count = 0;
-//   for (var index = 0; index < arrayOfCardObjects.length; index++) {
-//     var objectOfCards = arrayOfCardObjects[index];
-//       if (objectOfCards[keys] === 'ace') {
-//         count += 11;
-//       } else if (objectOfCards[keys] === 'jack' || objectOfCards[keys] === 'queen' || objectOfCards[keys] === 'king') {
-//         count += 10;
-//       } else {
-//         count += objectOfCards[keys];
-//       }
-//   }
-//   console.log(objectOfCards);
-//   return count;
-// }
+function calculateScore(shuffledDeck) {
+  var count = 0;
+  for (var index = 0; index < shuffledDeck.length; index++) {
+    if (shuffledDeck[index].rank === 'ace') {
+      count += 11;
+    } else if (shuffledDeck[index].rank === 'jack' || shuffledDeck[index].rank === 'queen' || shuffledDeck[index].rank === 'king') {
+      count += 10;
+    } else {
+      count += shuffledDeck[index].rank;
+    }
+  }
+  return count;
+}
 
 var competitors = ['Amelia', 'Louis', 'TJ', 'Cody', 'Tim'];
 var numOfCards1 = 6;
-// var numOfCards2 = 5;
-// var numOfCards3 = 7;
+var numOfCards2 = 5;
+var numOfCards3 = 7;
 
 cardGame(competitors, numOfCards1);
-// cardGame(competitors, numOfCards2);
-// cardGame(competitors, numOfCards3);
+cardGame(competitors, numOfCards2);
+cardGame(competitors, numOfCards3);
