5387893130650cddcbd88fb29b065210c58357d5
ameliatalkington <amrtalk@stanford.edu>
2021-02-15 14:08:59 -0700

    working on isAnagram function

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,29 +1,16 @@
 /* exported isAnagram */
 function isAnagram(firstString, secondString) {
   var isAnagram = true;
-  var firstStringNoSpace = '';
-  var secondStringNoSpace = '';
-  var noSpaceString1 = firstString.split(' ');
-  var noSpaceString2 = secondString.split(' ');
-  for (var index = 0; index < noSpaceString1.length; index++) {
-    firstStringNoSpace += noSpaceString1[index]
-  }
-  for (var i = 0; i < noSpaceString2.length; i++) {
-    secondStringNoSpace += noSpaceString2[i];
-  }
-
-  var string1Array = firstStringNoSpace.split('');
-  var sortedString1 = string1Array.sort();
-  var string2Array = secondStringNoSpace.split('');
-  var sortedString2 = string2Array.sort();
-
-  for (var j = 0; j < sortedString1.length; j++) {
-    if (sortedString1[j] === sortedString2[j]) {
-      isAnagram = true;
-    } else {
+  var noSpaceSecondString = secondString.split(' ').join('');
+  console.log(noSpaceSecondString);
+  for (var i = 0; i < noSpaceSecondString.length; i++) {
+    if (!firstString.includes(noSpaceSecondString[i])) {
       isAnagram = false;
       break;
+    } else {
+      isAnagram = true;
     }
+    noSpaceSecondString.slice(0, 1);
   }
   return isAnagram;
 }
