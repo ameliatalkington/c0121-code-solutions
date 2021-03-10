e4df4ec891461aa3e123733ad30ad1dc33097eac
ameliatalkington <amrtalk@stanford.edu>
2021-03-10 13:21:20 -0700

    Completed Array Manipulation 3 - AT

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,18 @@
 /* exported intersection */
+function intersection (first, second) {
+  var newArray = [];
+  if (first.length >= second.length) {
+    for (var i = 0; i < first.length; i++) {
+      if (second.includes(first[i])) {
+        newArray.push(first[i]);
+      }
+    }
+  } else {
+    for (var i = 0; i < second.length; i++) {
+      if (first.includes(second[i])) {
+        newArray.push(second[i]);
+      }
+    }
+  }
+  return newArray;
+}
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -2,7 +2,6 @@
 function union(first, second) {
   var newArray = [];
   var longArray = first.concat(second);
-  console.log(longArray);
   newArray = Array.from(new Set(longArray));
   return newArray;
 }
