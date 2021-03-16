bda65d5f13ceaf825b5980a9e1cf32237003b5b3
ameliatalkington <amrtalk@stanford.edu>
2021-03-16 22:08:48 +0000

    Express Post JSON - AT

---
+++ express-post-json/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+let nextID = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray = [];
+  for (id in grades) {
+    gradesArray.push(grades[id]);
+  }
+  res.json(gradesArray);
+});
+
+app.listen(3000, () => {
+  console.log('Listening to Port 3000!');
+});
+
+const parsedJSON = express.json();
+
+app.use(parsedJSON);
+
+app.post('/api/grades', (req, res) => {
+  if (!req.body) {
+    res.sendStatus(404);
+  } else {
+    req.body.id = nextID;
+    grades[nextID] = req.body;
+    console.log(grades);
+    nextID++;
+    res.status(201).send(req.body);
+  }
+});
