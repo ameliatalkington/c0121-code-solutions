6cfa5ea8fe810d83ca11eb7736be338cae71272e
ameliatalkington <amrtalk@stanford.edu>
2021-03-22 18:03:59 +0000

    Array Filter - AT

---
+++ array-filter/main.js
@@ -0,0 +1,49 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna'
+];
+
+const evenNumbers = numbers.filter(divisibleByTwo);
+console.log(evenNumbers);
+
+const overFive = numbers.filter(biggerThanFive);
+console.log(overFive);
+
+const startWithE = names.filter(firstLetterE);
+console.log(startWithE);
+
+const haveD = names.filter(containLetterD);
+console.log(haveD);
+
+function divisibleByTwo(number) {
+  if (number !== 0 && number % 2 === 0) {
+    return number;
+  }
+}
+
+function biggerThanFive(number) {
+  if (number > 5) {
+    return number;
+  }
+}
+
+function firstLetterE(name) {
+  if (name.charAt(0) === 'E') {
+    return name;
+  }
+}
+
+function containLetterD(name) {
+  if (name.includes('d') || name.includes('D')) {
+    return name;
+  }
+}
