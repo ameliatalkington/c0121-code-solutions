6b4e7a1a6e11c023f62e6ba2d0c0dba9a298b8b8
ameliatalkington <amrtalk@stanford.edu>
2021-04-02 17:48:54 +0000

    changes

--- javascript-closures/main.js
+++ javascript-closures/main.js
@@ -1,11 +1,8 @@
 let graduate = function (credential) {
   return function (fullName) {
-    console.log(`${fullName}, ${credential}`);
+    return `${fullName}, ${credential}`;
   }
 }
 
 let medicalSchool = graduate('M.D.');
 let lawSchool = graduate('Esq.');
-
-medicalSchool('Amelia Talkington');
-lawSchool('Ryan Florence');
